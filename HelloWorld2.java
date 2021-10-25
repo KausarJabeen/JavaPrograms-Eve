9c69b555182cacf3f272a400a8dc7c37266643d7
Kausar <48512652+KausarJabeen@users.noreply.github.com>
2021-10-25 22:45:32 +0530

    Static Input
    Static Input with String and Integer number datatye

---
+++ HelloWorld2.java
@@ -0,0 +1,17 @@
+package mod1.day1;
+/*
+ * Date: 12 Aug 2021 Thursday 
+ */
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myname="shoiab";//= (equalto) is a assignment operator
+		System.out.println("Welcome To JAVA...:"+myname);//+ is a concatenation operator
+		
+		int n1=10;
+		int n2=20;
+		System.out.println("The sum of two values...:"+(n1+n2));
+	}
+
+}
